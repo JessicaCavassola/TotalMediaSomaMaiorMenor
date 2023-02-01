a88373ef36d15da6f316ecb5ad3018fefa53e6f4
JessicaCavassola <114621485+JessicaCavassola@users.noreply.github.com>
2023-02-01 16:04:04 -0300

    Update analisador de numscript.js

--- analisador de numscript.js
+++ analisador de numscript.js
@@ -1,10 +1,7 @@
-/**
- * adicionar valores ao array e verificaçoes
- */
 
-let num = document.querySelector("input#fnum") /**manipulando o dom para capturar e armazenar o valor dado pelo usuario ja criando a variavel que o armazenara */
-let lista = document.querySelector ("select#flista")  /**manipulando o dom para capturar e armazenar os valores do selst ja criando a variavel que os armazenara */
-let res = document.querySelector ("div#res") /**manipulando o dom para capturar e armazenar o valor do resultado ja criando a variavel que o armazenara */
+let num = document.querySelector("input#fnum") 
+let lista = document.querySelector ("select#flista")  
+let res = document.querySelector ("div#res") 
 let valores = []
 
 /**função que verifica se o numero é valido */
@@ -27,8 +24,7 @@ function inLista(n, l){
 /**funcao que adiciona valores validos ao array */
 function adicionar(){
    if (isNumero (num.value) && !inLista (num.value, valores)) {
-        valores.push(Number(num.value)) /**neste ponto o valor ja esta sendo adicionado a lita porem nao esta sendo exibida ainda */
-         /**neste ponto iniciamos a inclusao visual na lista */
+        valores.push(Number(num.value))      
         let item = document.createElement("option")
         item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
@@ -36,13 +32,13 @@ function adicionar(){
    } else {
     window.alert ("valor invalido ou ja encontrado na lista")
    }
-   /**limpando  o input */
+   
    num.value = ""
    num.focus()
 }
 
 function finalizar(){
-    if  (valores.length == 0 ){ /**se o comprimenro do vetor for vazio */
+    if  (valores.length == 0 ){ 
         window.alert("adicione os  valores ante se finalizar")
     } else{
         let tot =  valores.length
@@ -56,8 +52,7 @@ function finalizar(){
             if (valores[pos] > maior)
                 maior = valores [pos]
             if (valores[pos] < menor)
-               menor = valores[pos]
-        
+               menor = valores[pos}      
 
         }
         media =  soma / tot
